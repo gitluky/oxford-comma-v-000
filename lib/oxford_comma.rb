11b0e47aa29ac18ac022048f6fd226c03eb84615
Jonathan Luk <jluk87@gmail.com>
2017-11-20 04:22:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
     array[0..1] = array[0..1].join(",")
     array[0..1].join("and")
   when array.lenght <= 2
-    array(0..1).join("and")
+    array[0..1].join("and")
   end
 end
